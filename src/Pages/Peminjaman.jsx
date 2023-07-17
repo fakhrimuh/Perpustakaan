aeb5ecb27a11f6aa10bfcaf52fd410f9ee77ab9e
fakhrimuh <muhammad.fakhri1234@gmail.com>
2023-07-17 20:32:15 +0700

    add table

--- src/Pages/Peminjaman.jsx
+++ src/Pages/Peminjaman.jsx
@@ -4,49 +4,96 @@ import {
   Card,
   CardBody,
   Typography,
-  CardFooter,
 } from "@material-tailwind/react";
 import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+
+const TABLE_HEAD = ["No", "Judul", "Tanggal"];
+
+const TABLE_ROWS = [
+  {
+    name: "1",
+    job: "Kalkulus",
+    date: "23/04/23",
+  },
+  {
+    name: "2",
+    job: "Developer",
+    date: "23/05/24",
+  },
+];
 
 export default function PeminjamanPage() {
   return (
     <div>
-      <div className="text-center text-redPrimary mb-2">
-        <h1 className="font-bold ">Cek Peminjaman Buku</h1>
-        <p className="mx-auto font-medium w-1/2">
-          Selamat datang di halaman Cek Peminjaman Buku! Di sini Anda dapat
-          memeriksa status peminjaman buku Anda dengan mudah.
-        </p>
-      </div>
-      <div className="mx-auto w-1/2 mb-2 flex items-center">
-        <Input
-          type="number"
-          label="Masukan Nim"
-          color="red"
-          icon={<MagnifyingGlassIcon />}
-          className=""
-        />
-        <Button
-          variant="outlined"
-          className="text-center text-redPrimary w-1/4 mx-2 border-redPrimary hover:bg-redPrimary hover:text-creamSecondary"
-        >
-          Cek
-        </Button>
-      </div>
-      <Card className="mt-6 w-96 mb-5">
+      <Card className="my-6 mx-auto w-1/2 bg-redPrimary">
+        <div className="text-center text-creamSecondary mb-4">
+          <h1 className="font-bold ">Cek Peminjaman Buku</h1>
+          <p className="mx-auto  font-medium">
+            Selamat datang di halaman Cek Peminjaman Buku! Di sini Anda dapat
+            memeriksa status peminjaman buku Anda dengan mudah.
+          </p>
+        </div>
+        <div className="w-1/2 mb-2 flex items-center mx-auto">
+          <Input
+            type="number"
+            label="Masukan Nim"
+            icon={<MagnifyingGlassIcon className="text-creamSecondary" />}
+            className="text-creamSecondary border-creamSecondary border-2 "
+          />
+          <Button
+            variant="outlined"
+            className="text-center hover:text-redPrimary w-1/4 mx-2 border-creamSecondary hover:bg-creamSecondary text-creamSecondary"
+          >
+            Cek
+          </Button>
+        </div>
+
         <CardBody>
-          <Typography variant="h5" color="blue-gray" className="mb-2">
-            UI/UX Review Check
+          <Typography variant="h5" className="mb-2 text-creamSecondary">
+            Muhammad Fakhri Febriawan
           </Typography>
-          <Typography>
-            The place is close to Barceloneta Beach and bus stop just 2 min by
-            walk and near to &quot;Naviglio&quot; where you can enjoy the main
-            night life in Barcelona.
-          </Typography>
+          <table className="w-full min-w-max bg-creamSecondary table-auto text-redPrimary text-left">
+            <thead>
+              <tr>
+                {TABLE_HEAD.map((head) => (
+                  <th key={head} className="border-b border-redPrimary p-4">
+                    <Typography className="font-bold leading-none">
+                      {head}
+                    </Typography>
+                  </th>
+                ))}
+              </tr>
+            </thead>
+            <tbody>
+              {TABLE_ROWS.map(({ name, job, date }, index) => {
+                const isLast = index === TABLE_ROWS.length - 1;
+                const classes = isLast
+                  ? "p-4"
+                  : "p-4 border-b border-redPrimary";
+
+                return (
+                  <tr key={name}>
+                    <td className={classes}>
+                      <Typography variant="small" className="font-semibold">
+                        {name}
+                      </Typography>
+                    </td>
+                    <td className={classes}>
+                      <Typography variant="small" className="font-semibold">
+                        {job}
+                      </Typography>
+                    </td>
+                    <td className={classes}>
+                      <Typography variant="small" className="font-semibold">
+                        {date}
+                      </Typography>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
         </CardBody>
-        <CardFooter className="pt-0">
-          <Button>Read More</Button>
-        </CardFooter>
       </Card>
     </div>
   );
