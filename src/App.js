d1c1f9db5fb0c4f2fc8416686c178bda27584223
farishuwaidi <farishuwaidi51@gmail.com>
2023-07-19 19:23:08 +0700

    add route

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import { Route, Routes } from "react-router-dom";
-import HomePage from "./Pages/Homepage";
-import HomeFooter from "./Components/Footer";
-import HeaderNavbar from "./Components/Navbar";
-import NewBookPage from "./Pages/NewBook";
-import PeminjamanPage from "./Pages/Peminjaman";
+import { Route, Routes } from 'react-router-dom';
+import HomePage from './Pages/Homepage';
+import HomeFooter from './Components/Footer';
+import HeaderNavbar from './Components/Navbar';
+import NewBookPage from './Pages/NewBook';
+import PeminjamanPage from './Pages/Peminjaman';
+import EJournalPage from './Pages/EJournalPage';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/usulanbuku" element={<NewBookPage />} />
         <Route path="/cekpeminjaman" element={<PeminjamanPage />} />
+        <Route path="/E-Journal" element={<EJournalPage />} />
       </Routes>
       <HomeFooter />
     </div>
