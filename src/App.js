6f674f69e693b737ec8aaf71eeac41a6426c6656
fakhrimuh <muhammad.fakhri1234@gmail.com>
2023-07-22 11:03:56 +0700

    Add route link

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import { Route, Routes } from 'react-router-dom';
-import HomePage from './Pages/Homepage';
-import HomeFooter from './Components/Footer';
-import HeaderNavbar from './Components/Navbar';
-import NewBookPage from './Pages/NewBook';
-import PeminjamanPage from './Pages/Peminjaman';
-import EJournalPage from './Pages/EJournalPage';
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./Pages/Homepage";
+import HomeFooter from "./Components/Footer";
+import HeaderNavbar from "./Components/Navbar";
+import NewBookPage from "./Pages/NewBook";
+import PeminjamanPage from "./Pages/Peminjaman";
+import EJournalPage from "./Pages/EJournalPage";
+import Kalista from "./Pages/Kalista";
+import OpacPage from "./Pages/OpacPage";
+import Upiana from "./Pages/UpianaPage";
+import DigitalRepositary from "./Pages/DigitalRepoPage";
 
 function App() {
   return (
@@ -15,6 +19,10 @@ function App() {
         <Route path="/usulanbuku" element={<NewBookPage />} />
         <Route path="/cekpeminjaman" element={<PeminjamanPage />} />
         <Route path="/E-Journal" element={<EJournalPage />} />
+        <Route path="/Upiana" element={<Upiana />} />
+        <Route path="/Opac" element={<OpacPage />} />
+        <Route path="/Kalista" element={<Kalista />} />
+        <Route path="/DigitalRepository" element={<DigitalRepositary />} />
       </Routes>
       <HomeFooter />
     </div>
