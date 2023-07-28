7788e96b6c5a67477a8f17a549eee233e623b85f
fakhrimuh <muhammad.fakhri1234@gmail.com>
2023-07-28 20:38:48 +0700

    fixing table

--- src/Pages/EJournalPage.jsx
+++ src/Pages/EJournalPage.jsx
@@ -1,65 +1,42 @@
-import { PencilIcon } from '@heroicons/react/24/solid';
-import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
-import { Card, CardHeader, Typography, Button, CardBody, Chip, CardFooter, Avatar, IconButton, Tooltip, Input } from '@material-tailwind/react';
 
-const TABLE_HEAD = ['Transaction', 'Amount', 'Date', 'Status', 'Account', ''];
+import { Card, CardHeader, Typography, CardBody, Chip, Avatar, } from '@material-tailwind/react';
+
+const TABLE_HEAD = ['Transaction', 'Amount', 'Date'];
 
 const TABLE_ROWS = [
   {
-    img: '/img/logos/logo-spotify.svg',
     name: 'Spotify',
-    amount: '$2,500',
-    date: 'Wed 3:00pm',
     status: 'paid',
     account: 'visa',
-    accountNumber: '1234',
-    expiry: '06/2026',
   },
   {
-    img: '/img/logos/logo-amazon.svg',
     name: 'Amazon',
-    amount: '$5,000',
-    date: 'Wed 1:00pm',
     status: 'paid',
     account: 'master-card',
-    accountNumber: '1234',
-    expiry: '06/2026',
   },
   {
-    img: '/img/logos/logo-pinterest.svg',
     name: 'Pinterest',
-    amount: '$3,400',
-    date: 'Mon 7:40pm',
     status: 'pending',
     account: 'master-card',
-    accountNumber: '1234',
-    expiry: '06/2026',
+
   },
   {
-    img: '/img/logos/logo-google.svg',
     name: 'Google',
-    amount: '$1,000',
-    date: 'Wed 5:00pm',
     status: 'paid',
     account: 'visa',
-    accountNumber: '1234',
-    expiry: '06/2026',
+
   },
   {
-    img: '/img/logos/logo-netflix.svg',
     name: 'netflix',
-    amount: '$14,000',
-    date: 'Wed 3:30am',
     status: 'cancelled',
     account: 'visa',
-    accountNumber: '1234',
-    expiry: '06/2026',
+
   },
 ];
 
 export default function EJournalPage() {
   return (
-    <div>
+    <div className="container mx-auto">
       <div className="my-4 text-center">
         <Typography variant="h5" className="text-redPrimary font-bold">
           E-Journal dan E-Book
@@ -68,26 +45,17 @@ export default function EJournalPage() {
           Ini akan menjadi deskripsi
         </Typography>
       </div>
-      <Card className="h-full w-3/4 mx-auto">
+      <Card className="h-full mx-auto mb-4">
         <CardHeader floated={false} shadow={false} className="rounded-none">
-          <div className="flex justify-end w-full">
-            <div className="content-end flex w-full shrink-0 gap-2 md:w-max">
-              <div className="w-full md:w-72">
-                <Input label="Search" icon={<MagnifyingGlassIcon className="h-5 w-5" />} />
-              </div>
-              <Button className="flex items-center gap-3" color="blue" size="sm">
-                Download
-              </Button>
-            </div>
-          </div>
+
         </CardHeader>
         <CardBody className=" px-0">
-          <table className="w-full min-w-max table-auto text-left">
+          <table className="w-11/12 min-w-max mx-auto table-auto text-left">
             <thead>
               <tr>
                 {TABLE_HEAD.map((head) => (
                   <th key={head} className="border-y border-blue-gray-100 bg-blue-gray-50/50 p-4">
-                    <Typography variant="small" color="blue-gray" className="font-normal leading-none opacity-70">
+                    <Typography variant="small" color="blue-gray" className="font-bold leading-none ">
                       {head}
                     </Typography>
                   </th>
@@ -103,21 +71,10 @@ export default function EJournalPage() {
                   <tr key={name}>
                     <td className={classes}>
                       <div className="flex items-center gap-3">
-                        <Avatar src={img} alt={name} size="md" className="border border-blue-gray-50 bg-blue-gray-50/50 object-contain p-1" />
                         <Typography variant="small" color="blue-gray" className="font-bold">
                           {name}
                         </Typography>
                       </div>
-                    </td>
-                    <td className={classes}>
-                      <Typography variant="small" color="blue-gray" className="font-normal">
-                        {amount}
-                      </Typography>
-                    </td>
-                    <td className={classes}>
-                      <Typography variant="small" color="blue-gray" className="font-normal">
-                        {date}
-                      </Typography>
                     </td>
                     <td className={classes}>
                       <div className="w-max">
@@ -135,22 +92,8 @@ export default function EJournalPage() {
                             className="h-full w-full object-contain p-1"
                           />
                         </div>
-                        <div className="flex flex-col">
-                          <Typography variant="small" color="blue-gray" className="font-normal capitalize">
-                            {account.split('-').join(' ')} {accountNumber}
-                          </Typography>
-                          <Typography variant="small" color="blue-gray" className="font-normal opacity-70">
-                            {expiry}
-                          </Typography>
-                        </div>
+
                       </div>
-                    </td>
-                    <td className={classes}>
-                      <Tooltip content="Edit User">
-                        <IconButton variant="text" color="blue-gray">
-                          <PencilIcon className="h-4 w-4" />
-                        </IconButton>
-                      </Tooltip>
                     </td>
                   </tr>
                 );
@@ -158,39 +101,7 @@ export default function EJournalPage() {
             </tbody>
           </table>
         </CardBody>
-        <CardFooter className="flex items-center justify-between border-t border-blue-gray-50 p-4">
-          <Button variant="outlined" color="blue-gray" size="sm">
-            Previous
-          </Button>
-          <div className="flex items-center gap-2">
-            <IconButton variant="outlined" color="blue-gray" size="sm">
-              1
-            </IconButton>
-            <IconButton variant="text" color="blue-gray" size="sm">
-              2
-            </IconButton>
-            <IconButton variant="text" color="blue-gray" size="sm">
-              3
-            </IconButton>
-            <IconButton variant="text" color="blue-gray" size="sm">
-              ...
-            </IconButton>
-            <IconButton variant="text" color="blue-gray" size="sm">
-              8
-            </IconButton>
-            <IconButton variant="text" color="blue-gray" size="sm">
-              9
-            </IconButton>
-            <IconButton variant="text" color="blue-gray" size="sm">
-              10
-            </IconButton>
-          </div>
-          <Button variant="outlined" color="blue-gray" size="sm">
-            Next
-          </Button>
-        </CardFooter>
       </Card>
-      );
     </div>
   );
 }
