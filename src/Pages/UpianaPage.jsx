c3d0a0a16b889a23ee169ad15a6e03fa896ae919
fakhrimuh <muhammad.fakhri1234@gmail.com>
2023-09-11 15:51:18 +0700

    add page

--- src/Pages/UpianaPage.jsx
+++ src/Pages/UpianaPage.jsx
@@ -1,3 +1,194 @@
+import { MagnifyingGlassIcon, ChevronUpDownIcon } from "@heroicons/react/24/outline";
+import {
+  Card,
+  CardHeader,
+  Input,
+  Typography,
+  Button,
+  CardBody,
+  Chip,
+  CardFooter,
+  Avatar
+} from "@material-tailwind/react";
+
+
+
+const TABLE_HEAD = ["Nomor Panggil", "Penulis", "judul", "Anak Judul", "Topik 1", "Topik 2", "Topik 3"];
+
+const TABLE_ROWS = [
+  {
+    img: "https://demos.creative-tim.com/test/corporate-ui-dashboard/assets/img/team-3.jpg",
+    name: "John Michael",
+    email: "john@creative-tim.com",
+    job: "Manager",
+    org: "Organization",
+    online: true,
+    date: "23/04/18",
+  },
+  {
+    img: "https://demos.creative-tim.com/test/corporate-ui-dashboard/assets/img/team-2.jpg",
+    name: "Alexa Liras",
+    email: "alexa@creative-tim.com",
+    job: "Programator",
+    org: "Developer",
+    online: false,
+    date: "23/04/18",
+  },
+  {
+    img: "https://demos.creative-tim.com/test/corporate-ui-dashboard/assets/img/team-1.jpg",
+    name: "Laurent Perrier",
+    email: "laurent@creative-tim.com",
+    job: "Executive",
+    org: "Projects",
+    online: false,
+    date: "19/09/17",
+  },
+  {
+    img: "https://demos.creative-tim.com/test/corporate-ui-dashboard/assets/img/team-4.jpg",
+    name: "Michael Levi",
+    email: "michael@creative-tim.com",
+    job: "Programator",
+    org: "Developer",
+    online: true,
+    date: "24/12/08",
+  },
+  {
+    img: "https://demos.creative-tim.com/test/corporate-ui-dashboard/assets/img/team-5.jpg",
+    name: "Richard Gran",
+    email: "richard@creative-tim.com",
+    job: "Manager",
+    org: "Executive",
+    online: false,
+    date: "04/10/21",
+  },
+];
+
+
 export default function Upiana() {
-  return <h1>Upiana Page</h1>;
+  return (
+    <div className="container mx-auto">
+      <div className="my-4 text-center">
+        <Typography variant="h4" className="text-redPrimary font-bold">
+          E-Journal dan E-Book
+        </Typography>
+        <Typography variant="paragraph" className="my-2 w-3/4 mx-auto leading-normal font-medium text-redPrimary text-justify">
+          Temukan koleksi lengkap jurnal dan E-book dari berbagai bidang ilmu dengan akses mudah dan terpercaya. Jelajahi artikel terkini dan kumpulan E-book untuk meningkatkan pengetahuan dan pemahaman Anda. Dapatkan manfaatnya dengan mengunduh E-book favorit Anda dan tetap terhubung dengan perkembangan terkini dalam dunia penelitian. Mari mulai petualangan membaca dengan konten yang menginspirasi dan mendidik.
+        </Typography>
+      </div>
+      <Card className="h-full mx-auto w-5/6 mb-4">
+        <CardHeader floated={false} shadow={false} className="rounded-none">
+          <div className="flex flex-col items-center justify-between gap-4 md:flex-row">
+
+            <div className="w-full md:w-72">
+              <Input label="Search" icon={<MagnifyingGlassIcon className="h-5 w-5" />} />
+            </div>
+          </div>
+        </CardHeader>
+        <CardBody className=" px-0">
+          <table className="bg-creamPrimary border-redPrimary w-3/4 mx-auto min-w-max table-auto text-left rounded-lg">
+            <thead>
+              <tr>
+                {TABLE_HEAD.map((head, index) => (
+                  <th
+                    key={head}
+                    className="cursor-pointer border-redPrimary bg-creamSecondary p-4 transition-colors hover:bg-blue-gray-50"
+                  >
+                    <Typography
+                      variant="small"
+                      className="flex items-center justify-between gap-2 font-bold leading-none text-redPrimary"
+                    >
+                      {head}  <ChevronUpDownIcon strokeWidth={2} className="h-4 w-4" />
+                    </Typography>
+                  </th>
+                ))}
+              </tr>
+            </thead>
+            <tbody className="overflow-hidden">
+              {TABLE_ROWS.map(({ img, name, email, job, org, online, date }, index) => {
+                const isLast = index === TABLE_ROWS.length - 1;
+                const classes = isLast ? "p-4" : "p-4 border-b border-blue-gray-50";
+
+                return (
+                  <tr key={name} className="even:bg-creamSecondary">
+                    <td className={classes}>
+                      <div className="flex items-center gap-3">
+                        <Avatar src={img} alt={name} size="sm" />
+                        <div className="flex flex-col">
+                          <Typography variant="small" color="blue-gray" className="font-normal">
+                            {name}
+                          </Typography>
+                          <Typography
+                            variant="small"
+                            color="blue-gray"
+                            className="font-normal opacity-70"
+                          >
+                            {email}
+                          </Typography>
+                        </div>
+                      </div>
+                    </td>
+                    <td className={classes}>
+                      <div className="flex flex-col">
+                        <Typography variant="small" color="blue-gray" className="font-normal">
+                          {job}
+                        </Typography>
+                        <Typography
+                          variant="small"
+                          color="blue-gray"
+                          className="font-normal opacity-70"
+                        >
+                          {org}
+                        </Typography>
+                      </div>
+                    </td>
+                    <td className={classes}>
+                      <div className="w-max">
+                        <Chip
+                          variant="ghost"
+                          size="sm"
+                          value={online ? "online" : "offline"}
+                          color={online ? "green" : "blue-gray"}
+                        />
+                      </div>
+                    </td>
+                    <td className={classes}>
+                      <Typography variant="small" color="blue-gray" className="font-normal">
+                        {date}
+                      </Typography>
+                    </td>
+                    <td className={classes}>
+                      <Typography variant="small" color="blue-gray" className="font-normal">
+                        {date}
+                      </Typography>
+                    </td><td className={classes}>
+                      <Typography variant="small" color="blue-gray" className="font-normal">
+                        {date}
+                      </Typography>
+                    </td><td className={classes}>
+                      <Typography variant="small" color="blue-gray" className="font-normal">
+                        {date}
+                      </Typography>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </CardBody>
+        <CardFooter className="flex items-center justify-between border-t border-blue-gray-50 p-4">
+          <Typography variant="small" color="blue-gray" className="font-normal">
+            Page 1 of 10
+          </Typography>
+          <div className="flex gap-2">
+            <Button variant="outlined" color="blue-gray" size="sm">
+              Previous
+            </Button>
+            <Button variant="outlined" color="blue-gray" size="sm">
+              Next
+            </Button>
+          </div>
+        </CardFooter>
+      </Card>
+    </div>
+  );
 }
