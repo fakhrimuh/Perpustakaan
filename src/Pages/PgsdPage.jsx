3cd2cc823c64372fb3ac1ccdce174c7ff83d8d24
fakhrimuh <muhammad.fakhri1234@gmail.com>
2024-01-04 08:07:47 +0700

    adding PGSD Page

---
+++ src/Pages/PgsdPage.jsx
@@ -0,0 +1,360 @@
+import { Input, Typography, Card, CardHeader, CardBody } from '@material-tailwind/react';
+import { Link } from 'react-router-dom';
+import FakultasList from '../Components/FakultasList';
+import { UserIcon, ArchiveBoxIcon } from '@heroicons/react/24/outline';
+import React, { useState, useMemo } from 'react';
+
+const dataJson = {
+  1: {
+    link: 'http://repository.upi.edu/97086/',
+    judul: 'Pengaruh Penggunaan Media Pembelajaran Interaktif Terhadap Peningkatan Kemampuan Membaca Siswa Kelas II SD',
+    penulis: 'John Doe',
+    nim: '123456',
+    tahun: 2022,
+    jenis: 'Tesis',
+  },
+  2: {
+    link: 'http://repository.upi.edu/97086/',
+    judul: 'Implementasi Pendekatan Saintifik dalam Pembelajaran IPA untuk Meningkatkan Keterampilan Proses Siswa Kelas IV SD',
+    penulis: 'Jane Doe',
+    nim: '654321',
+    tahun: 2021,
+    jenis: 'Sistem Informasi',
+  },
+  3: {
+    link: 'http://repository.upi.edu/97086/',
+    judul: 'Pengembangan Bahan Ajar Matematika Berbasis Kearifan Lokal Untuk Meningkatkan Minat Belajar Siswa SD',
+    penulis: 'Bob Johnson',
+    nim: '987654',
+    tahun: 2023,
+    jenis: 'Pengujian',
+  },
+  4: {
+    link: 'http://repository.upi.edu/97086/',
+    judul: 'Efektivitas Penggunaan Teknologi Augmented Reality dalam Pembelajaran Bahasa Inggris di Sekolah Dasar',
+    penulis: 'Alice Wonderland',
+    nim: '456789',
+    tahun: 2022,
+    jenis: 'Aplikasi',
+  },
+  5: {
+    link: 'http://repository.upi.edu/97086/',
+    judul: 'Peningkatan Hasil Belajar Matematika melalui Penerapan Model Pembelajaran Kooperatif Tipe Jigsaw pada Siswa Kelas V SD',
+    penulis: 'Charlie Brown',
+    nim: '321654',
+    tahun: 2023,
+    jenis: 'Sistem Informasi',
+  },
+  6: {
+    link: 'http://repository.upi.edu/97086/',
+    judul: 'Evaluasi Implementasi Kurikulum 2013 dalam Pembelajaran Tematik di Sekolah Dasar',
+    penulis: 'Eva Green',
+    nim: '987123',
+    tahun: 2021,
+    jenis: 'Pengembangan Permainan',
+  },
+  7: {
+    link: 'http://repository.upi.edu/97086/',
+    judul: 'Pengaruh Penggunaan Metode Peer Tutoring Terhadap Kemandirian Belajar Siswa SD',
+    penulis: 'David Copperfield',
+    nim: '111222',
+    tahun: 2022,
+    jenis: 'Sistem Informasi',
+  },
+  8: {
+    link: 'http://repository.upi.edu/97086/',
+    judul: 'Pengembangan Modul Pembelajaran Berbasis Proyek untuk Meningkatkan Kreativitas Siswa Sekolah Dasar',
+    penulis: 'Grace Kelly',
+    nim: '333444',
+    tahun: 2023,
+    jenis: 'Aplikasi',
+  },
+  9: {
+    link: 'http://repository.upi.edu/97086/',
+    judul: 'Analisis Kesulitan Belajar Matematika pada Siswa Sekolah Dasar dan Strategi Mengatasinya',
+    penulis: 'George Orwell',
+    nim: '555666',
+    tahun: 2021,
+    jenis: 'Pengembangan Web',
+  },
+  10: {
+    link: 'http://repository.upi.edu/97086/',
+    judul: 'Pengaruh Keterlibatan Orang Tua dalam Pembelajaran Anak di Sekolah Dasar',
+    penulis: 'Mary Shelley',
+    nim: '777888',
+    tahun: 2022,
+    jenis: 'Sistem Informasi',
+  },
+  11: {
+    link: 'http://repository.upi.edu/97086/',
+    judul: 'Pengembangan Buku Cerita Interaktif sebagai Media Pembelajaran Bahasa Indonesia di Kelas I SD',
+    penulis: 'Farhan Maulana',
+    nim: '123456',
+    tahun: 2022,
+    jenis: 'Aplikasi',
+  },
+  12: {
+    link: 'http://repository.upi.edu/97086/',
+    judul: 'Evaluasi Implementasi Pembelajaran Tematik pada Siswa Kelas III SD dalam Perspektif Kesiapan Guru',
+    penulis: 'Dina Pratiwi',
+    nim: '654321',
+    tahun: 2021,
+    jenis: 'Sistem Informasi',
+  },
+  13: {
+    link: 'http://repository.upi.edu/97086/',
+    judul: 'Pemanfaatan Teknologi Mobile dalam Meningkatkan Keterampilan Menulis Siswa Kelas IV SD',
+    penulis: 'Yoga Prakasa',
+    nim: '987654',
+    tahun: 2023,
+    jenis: 'Pengujian',
+  },
+  14: {
+    link: 'http://repository.upi.edu/97086/',
+    judul: 'Analisis Efektivitas Penggunaan Permainan Edukatif dalam Pembelajaran Matematika di Sekolah Dasar',
+    penulis: 'Maya Sari',
+    nim: '456789',
+    tahun: 2022,
+    jenis: 'Aplikasi',
+  },
+  15: {
+    link: 'http://repository.upi.edu/97086/',
+    judul: 'Pengaruh Lingkungan Sekolah dan Dukungan Keluarga terhadap Prestasi Belajar Siswa SD',
+    penulis: 'Agus Susanto',
+    nim: '321654',
+    tahun: 2023,
+    jenis: 'Sistem Informasi',
+  },
+  16: {
+    link: 'http://repository.upi.edu/97086/',
+    judul: 'Pengembangan Model Pembelajaran Inklusif untuk Siswa Berkebutuhan Khusus di Sekolah Dasar',
+    penulis: 'Indah Lestari',
+    nim: '987123',
+    tahun: 2021,
+    jenis: 'Pengembangan Permainan',
+  },
+  17: {
+    link: 'http://repository.upi.edu/97086/',
+    judul: 'Implementasi Metode Project-Based Learning dalam Pembelajaran IPA untuk Siswa Kelas V SD',
+    penulis: 'Rizki Ramadhan',
+    nim: '111222',
+    tahun: 2022,
+    jenis: 'Sistem Informasi',
+  },
+  18: {
+    link: 'http://repository.upi.edu/97086/',
+    judul: 'Pengaruh Pembelajaran Berbasis Proyek terhadap Kreativitas Anak dalam Menyelesaikan Tugas di Kelas II SD',
+    penulis: 'Mega Indriani',
+    nim: '333444',
+    tahun: 2023,
+    jenis: 'Aplikasi',
+  },
+  19: {
+    link: 'http://repository.upi.edu/97086/',
+    judul: 'Penerapan Pendekatan Saintifik dalam Pembelajaran Seni Budaya untuk Meningkatkan Keterampilan Berpikir Kritis Siswa SD',
+    penulis: 'Aditya Pratama',
+    nim: '555666',
+    tahun: 2021,
+    jenis: 'Pengembangan Web',
+  },
+  20: {
+    link: 'http://repository.upi.edu/97086/',
+    judul: 'Pengaruh Penggunaan Permainan Edukatif terhadap Minat Belajar Matematika pada Anak Kelas I SD',
+    penulis: 'Novi Fitriyah',
+    nim: '777888',
+    tahun: 2022,
+    jenis: 'Sistem Informasi',
+  },
+};
+
+export default function PgsdPage() {
+  const [currentPage, setCurrentPage] = useState(1);
+  const [itemsPerPage, setItemsPerPage] = useState(5);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [searchedData, setSearchedData] = useState([]);
+  const [isSearching, setIsSearching] = useState(false);
+  const totalItems = Object.keys(dataJson).length;
+
+  const filteredData = useMemo(() => {
+    return isSearching ? searchedData : Object.values(dataJson);
+  }, [isSearching, searchedData]);
+
+  const handleSearch = () => {
+    if (searchTerm.trim() !== '') {
+      const term = searchTerm.toLowerCase();
+      const results = Object.values(dataJson).filter((item) => item.judul.toLowerCase().includes(term) || item.penulis.toLowerCase().includes(term) || item.jenis.toLowerCase().includes(term));
+      setSearchedData(results);
+      setIsSearching(true);
+      setCurrentPage(1);
+    } else {
+      setIsSearching(false);
+    }
+  };
+
+  const totalPages = Math.ceil(filteredData.length / itemsPerPage);
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentItems = filteredData.slice(indexOfFirstItem, indexOfLastItem);
+
+  const nextPage = () => {
+    setCurrentPage((prevPage) => (prevPage === totalPages ? prevPage : prevPage + 1));
+  };
+
+  const prevPage = () => {
+    setCurrentPage((prevPage) => (prevPage === 1 ? prevPage : prevPage - 1));
+  };
+
+  const handleItemsPerPageChange = (e) => {
+    const newItemsPerPage = parseInt(e.target.value, 10);
+    setItemsPerPage(newItemsPerPage);
+    setCurrentPage(1);
+  };
+
+  return (
+    <div className="text-center mt-[110px]">
+      {/* Title and Search bar */}
+      <div className="mt-28">
+        <h1 className="text-3xl font-extrabold text-redPrimary mb-6">Pendidikan Guru Sekolah Dasar</h1>
+        <div>
+          <div className="w-1/2 mx-auto mb-16 bg-redPrimary border-solid border-2 border-creamSecondary py-4 px-8 rounded-lg">
+            <div className="flex gap-2 ">
+              <Input
+                type="text"
+                placeholder="Kata Kunci"
+                onChange={(e) => setSearchTerm(e.target.value)}
+                value={searchTerm}
+                className="!border-2 !border-creamSecondary  text-creamSecondary  placeholder-creamSecondary"
+                labelProps={{
+                  className: 'hidden',
+                }}
+              />
+              <button onClick={handleSearch} className="bg-creamSecondary font-semibold text-redPrimary border-2 px-6 rounded-lg hover:bg-redPrimary hover:text-creamSecondary">
+                Cari
+              </button>
+            </div>
+            <div className="bg-creamSecondary w-fit px-4 mt-3 rounded-md">
+              <Typography className="text-redPrimary font-semibold text-sm my-2">Pendidikan Guru Sekolah Dasar</Typography>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* Body */}
+      <div className="container flex gap-4 mx-auto mb-6">
+        {/* Daftar Jurnal */}
+        <div className="w-3/4 border border-redPrimary rounded-lg">
+          {currentItems.map((book, index) => {
+            const bookIndex = indexOfFirstItem + index + 1;
+            return (
+              <Link to={book.link} target="_blank" key={index}>
+                <Card shadow={false} className="w-11/12 mx-auto flex-row bg-transparent border-b-2 border-redPrimary rounded-none mb-2">
+                  <CardHeader shadow={false} floated={false} className="m-0 w-1/5 bg-transparent shrink-0 rounded-r-none">
+                    <div className="bg-redPrimary rounded-lg w-10 h-10 py-2 mt-10 mx-auto">
+                      <Typography className="text-creamSecondary">{bookIndex}</Typography>
+                    </div>
+                  </CardHeader>
+                  <CardBody>
+                    <Typography color="black" className="mb-3 text-base font-semibold text-left">
+                      {book.judul}
+                    </Typography>
+                    <div className="flex gap-3 mb-2">
+                      <UserIcon className="w-5 h-5 text-redPrimary" />
+                      <Typography color="black" className="text-sm">
+                        {book.penulis} {book.nim} - {book.tahun}
+                      </Typography>
+                    </div>
+                    <div className="flex gap-3 mb-">
+                      <ArchiveBoxIcon className="w-5 h-5 text-redPrimary" />
+                      <Typography color="black" className="text-sm">
+                        Tesis
+                      </Typography>
+                    </div>
+                  </CardBody>
+                </Card>
+              </Link>
+            );
+          })}
+
+          {/* Pagination */}
+          <div className="container w-11/12 max-w-screen-lg mx-auto flex justify-between gap-6 mb-4">
+            <Typography className="text-left text-sm mt-2">
+              {indexOfFirstItem + 1} - {Math.min(indexOfLastItem, totalItems)} dari {filteredData.length} baris
+            </Typography>
+            <div className="flex">
+              <Typography className="text-sm mt-2">Jumlah baris per halaman</Typography>
+              <select value={itemsPerPage} onChange={handleItemsPerPageChange} className="mx-3 py-1 px-2 rounded bg-transparent border border-redPrimary text-redPrimary">
+                <option value={5}>5</option>
+                <option value={10}>10</option>
+                <option value={15}>15</option>
+              </select>
+            </div>
+
+            <div>
+              <button onClick={prevPage} disabled={currentPage === 1} className={`px-1 py-1 rounded ${currentPage === 1 ? 'text-gray-600' : 'bg-redPrimary text-creamSecondary hover:bg-redPrimary hover:text-creamSecondary'}`}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={2}
+                  stroke="currentColor"
+                  className="w-5 h-5 inline-block align-middle" // Tambahkan class atau style di sini
+                  style={{ verticalAlign: 'middle' }} // Contoh penggunaan inline style
+                >
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" />
+                </svg>
+              </button>
+              {Array.from({ length: totalPages }, (_, i) => (
+                <button
+                  key={i}
+                  onClick={() => setCurrentPage(i + 1)}
+                  className={`px-3 py-1 rounded ${currentPage === i + 1 ? 'bg-redPrimary text-creamSecondary' : 'bg-creamSecondary text-redPrimary hover:bg-redPrimary hover:text-creamSecondary'}`}
+                >
+                  {i + 1}
+                </button>
+              ))}
+              <button
+                onClick={nextPage}
+                disabled={currentPage === totalPages}
+                className={`px-1 py-1 rounded ${currentPage === totalPages ? ' text-gray-600' : 'bg-redPrimary text-creamSecondary hover:bg-redPrimary hover:text-creamSecondary'}`}
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={2}
+                  stroke="currentColor"
+                  className="w-5 h-5 inline-block align-middle" // Tambahkan class atau style di sini
+                  style={{ verticalAlign: 'middle' }} // Contoh penggunaan inline style
+                >
+                  <path strokeLinecap="round" strokeLinejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
+                </svg>
+              </button>
+            </div>
+          </div>
+        </div>
+
+        {/* Tabs Jurusan */}
+        <div className="w-1/4 border h-fit border-redPrimary rounded-lg">
+          <div className="mx-auto w-5/6 rounded-b-sm border-b-2 border-redPrimary my-2 mb-6">
+            <Typography className="text-redPrimary font-semibold mb-2 text-3xl">Program Studi</Typography>
+          </div>
+          {/* List Jurusan */}
+          <div>
+            <Link to='/JurusanPage'>
+              <button className="bg-creamSecondary text-redPrimary rounded-lg p-2 hover:bg-redPrimary hover:text-creamSecondary text-sm font-semibold mb-4">Rekayasa Perangkat Lunak - S1</button>
+            </Link>
+            <button className="rounded-lg p-2 bg-redPrimary text-creamSecondary text-sm font-semibold mb-4">Pendidikan Guru Sekolah Dasar - S1</button>
+            <button className="bg-creamSecondary text-redPrimary rounded-lg p-2 hover:bg-redPrimary hover:text-creamSecondary text-sm font-semibold mb-4">Pendidikan Guru PAUD - S1</button>
+            <button className="bg-creamSecondary text-redPrimary rounded-lg p-2 hover:bg-redPrimary hover:text-creamSecondary text-sm font-semibold mb-4">Pendidikan Multimedia - S1</button>
+            <button className="bg-creamSecondary text-redPrimary rounded-lg p-2 hover:bg-redPrimary hover:text-creamSecondary text-sm font-semibold mb-4">Teknik Komputer - S1</button>
+            <button className="bg-creamSecondary text-redPrimary rounded-lg p-2 hover:bg-redPrimary hover:text-creamSecondary text-sm font-semibold mb-4">Pedndikan Guru Sekolah Dasar - S2</button>
+          </div>
+        </div>
+      </div>
+
+      <div className="mt-10">
+        <FakultasList className="mx-auto" />
+      </div>
+    </div>
+  );
+}
