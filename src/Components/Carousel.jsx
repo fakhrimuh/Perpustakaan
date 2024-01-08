c85d76f5fd45293e25143d22e52eeb9e5231f891
farishuwaidi <farishuwaidi51@gmail.com>
2024-01-08 21:28:41 +0700

    change z-index

--- src/Components/Carousel.jsx
+++ src/Components/Carousel.jsx
@@ -1,26 +1,14 @@
-import { Carousel } from "@material-tailwind/react";
-import PicOne from "../Asset/Carousel/PicOne.png";
-import PicTwo from "../Asset/Carousel/PicTwo.png";
-import PicThree from "../Asset/Carousel/PicThree.png";
+import { Carousel } from '@material-tailwind/react';
+import PicOne from '../Asset/Carousel/PicOne.png';
+import PicTwo from '../Asset/Carousel/PicTwo.png';
+import PicThree from '../Asset/Carousel/PicThree.png';
 
 export default function CarouselHome() {
   return (
-    <Carousel>
-      <img
-        src={PicOne}
-        alt="#1"
-        className="h-full w-full object-cover rounded-xl"
-      />
-      <img
-        src={PicTwo}
-        alt="#2"
-        className="h-full w-full object-cover rounded-xl"
-      />
-      <img
-        src={PicThree}
-        alt="#2"
-        className="h-full w-full object-cover rounded-xl"
-      />
+    <Carousel className="rounded-xl z-20" transition={{ duration: 2 }}>
+      <img src={PicOne} alt="#1" className="h-full w-full object-cover rounded-xl" />
+      <img src={PicTwo} alt="#2" className="h-full w-full object-cover rounded-xl" />
+      <img src={PicThree} alt="#2" className="h-full w-full object-cover rounded-xl" />
     </Carousel>
   );
 }
